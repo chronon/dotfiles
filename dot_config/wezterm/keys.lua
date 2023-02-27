260139ed7d958f5eb5d614fd63d30b06077adbb7
Gregory Gaskill <gregory@chronon.com>
2023-02-27 14:30:19 -0500

    Add wezterm keys config

---
+++ dot_config/wezterm/keys.lua
@@ -0,0 +1,39 @@
+local wezterm = require('wezterm')
+local act = wezterm.action
+
+local keys = {
+  leader = {
+    key = 'a',
+    mods = 'CTRL',
+    timeout_milliseconds = 1000
+  },
+  keys = {
+    {
+      key = 'LeftArrow',
+      mods = 'CTRL|SHIFT',
+      action = act.ActivateTabRelative(-1),
+    },
+    {
+      key = 'RightArrow',
+      mods = 'CTRL|SHIFT',
+      action = act.ActivateTabRelative(1),
+    },
+    {
+      key = '-',
+      mods = 'LEADER',
+      action = act.SplitPane {
+        direction = 'Down',
+        size = { Percent = 25 },
+      },
+    },
+    {
+      key = '\\',
+      mods = 'LEADER',
+      action = act.SplitHorizontal {
+        domain='CurrentPaneDomain',
+      },
+    },
+  },
+}
+
+return keys
