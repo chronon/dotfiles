0d4db5f67a1fc3c43123d7c87a90b2dbd40d6a9a
Gregory Gaskill <gregory@chronon.com>
2023-05-14 07:45:38 -0400

    Disable Javascript auto format

--- dot_config/nvim/lua/config/autocmds.lua
+++ dot_config/nvim/lua/config/autocmds.lua
@@ -2,7 +2,7 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 
 vim.api.nvim_create_autocmd({ "FileType" }, {
-  pattern = { "php" },
+  pattern = { "php", "javascript" },
   callback = function()
     vim.b.autoformat = false
   end,
